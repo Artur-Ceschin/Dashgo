d636ad97795631ccb448881ab96287044f875baf
Artur Ceschin <artur.ceschin@gmail.com>
2022-01-03 22:31:24 -0300

    Pagination started

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,16 +12,20 @@ import {
     Tbody,
     Td,
     Text,
+    Spinner,
     useBreakpointValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/SideBar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
+    const { data, isLoading, error, isFetching } = useUsers();
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -35,6 +39,9 @@ export default function UserList() {
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">
                             Usuários
+                            {!isLoading && isFetching && (
+                                <Spinner size="sm" color="gray.500" ml="4" />
+                            )}
                         </Heading>
                         <Link href="/users/create" passHref>
                             <Button
@@ -47,72 +54,65 @@ export default function UserList() {
                             </Button>
                         </Link>
                     </Flex>
-                    <Table>
-                        <Thead>
-                            <Tr>
-                                <Th
-                                    px={["4", "4", "6"]}
-                                    color="gray.300"
-                                    width="8"
-                                >
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">
-                                            Artur Ceschin
-                                        </Text>
-                                        <Text fontSize="sm" color="gray.300">
-                                            artur.ceschin@gmail.com
-                                        </Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Agosto, 2021</Td>}
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">
-                                            Artur Ceschin
-                                        </Text>
-                                        <Text fontSize="sm" color="gray.300">
-                                            artur.ceschin@gmail.com
-                                        </Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Agosto, 2021</Td>}
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">
-                                            Artur Ceschin
-                                        </Text>
-                                        <Text fontSize="sm" color="gray.300">
-                                            artur.ceschin@gmail.com
-                                        </Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Agosto, 2021</Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados do usuários</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table>
+                                <Thead>
+                                    <Tr>
+                                        <Th
+                                            px={["4", "4", "6"]}
+                                            color="gray.300"
+                                            width="8"
+                                        >
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && (
+                                            <Th>Data de cadastro</Th>
+                                        )}
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map((user) => (
+                                        <Tr key={user.id}>
+                                            <Td px={["4", "4", "6"]}>
+                                                <Checkbox colorScheme="pink" />
+                                            </Td>
+                                            <Td>
+                                                <Box>
+                                                    <Text fontWeight="bold">
+                                                        {user.name}
+                                                    </Text>
+                                                    <Text
+                                                        fontSize="sm"
+                                                        color="gray.300"
+                                                    >
+                                                        {user.email}
+                                                    </Text>
+                                                </Box>
+                                            </Td>
+                                            {isWideVersion && (
+                                                <Td>{user.createdAt}</Td>
+                                            )}
+                                        </Tr>
+                                    ))}
+                                </Tbody>
+                            </Table>
+                            <Pagination
+                                totalCountOfRegisters={200}
+                                currentPage={5}
+                                onPageChange={() => {}}
+                            />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
